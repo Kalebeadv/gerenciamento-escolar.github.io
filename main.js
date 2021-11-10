1fe383324c6b4d66559bd3735fb81615445fb126
kalebe <kalebeadv66@gmail.com>
2021-11-10 12:00:48 -0500

    primeiro commit

---
+++ main.js
@@ -0,0 +1,25 @@
+class User
+{
+    constructor(id,name,email,password,bith_date,sex)
+    {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.bith_date = bith_date;
+        this.sex = sex;
+    }
+
+    userShow()
+    {
+        console.log(`${this.id,this.name,this.email,this.password,this.bith_date,this.sex}`)
+    }
+}
+
+function the_student()
+{
+     window.alert("opa")
+}
+
+
+
