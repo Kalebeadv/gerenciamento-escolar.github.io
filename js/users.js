c3e446b3003b928216ecde89aa8994f4c2ce10e4
kalebe <kalebeadv66@gmail.com>
2021-11-19 09:14:03 -0500

    criei duas classes, users e employee. estou trabalhando em employee

---
+++ js/users.js
@@ -0,0 +1,10 @@
+export class Users
+{
+    constructor(id,name,bith_date,sex)
+    {
+        this.id = id;
+        this.name = name;
+        this.bith_date = bith_date;
+        this.sex = sex;
+    }
+}
