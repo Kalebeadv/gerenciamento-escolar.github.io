83315713aedd5687ba54811c94b5319e1c773cb7
kalebe <kalebeadv66@gmail.com>
2021-11-20 15:28:31 -0300

    Quase finalizando a classe employee

--- js/employees.js
+++ js/employees.js
@@ -1,6 +1,6 @@
 import { Users } from "./users.js"
 
-class Employees extends Users {
+export class Employees extends Users {
     constructor(id, user_id, name, bith_date, sex, type_of_employee, cpf, status) {
         super(user_id, name, bith_date, sex);
         this.id = id;
@@ -9,37 +9,51 @@ class Employees extends Users {
         this.status = status;
     }
 
-    registration()
-    {
-
-    }
-    display_report()
-    {
-        
-    }
 }
 
-var employees = [];
-let button_birth_day = document.getElementById("button_birth_day");
+export const employees_guard = [employ1,employ2,employ3,employ4];
 let button_registration = document.getElementById("button_registration");
+let button_show_birthdays = document.getElementById("button_employee_birthday");
 
-button_registration.addEventListener("click", fetchIssues);
-button_birth_day.addEventListener("click", show_birthdays);
+button_registration.addEventListener("click", registration);
 
-function registrationStorage()
-{
-    localStorage.setItem("id", document.getElementById("id").value);
-    localStorate.setItem("user_id", document.getElementById("user_id"),value);
-    localStorage.setItem("name", document.getElementById("name").value);
-    localStorage.setItem("bith_date", document.getElementById("bith_date").value);
-    localStorage.setItem("sex", document.getElementById("sex").value);
-    localStorage.setItem("type", document.getElementById("type").value);
-    localStorage.setItem("cpf", document.getElementById("cpf").value); 
-    localStorage.setItem("status", document.getElementById("status").value);
-     
+button_show_birthdays.addEventListener("click", display_report_birthday);
+
+
+var employ1 = new Employees("1", "1", "kalebe", "03/12/1960", "M", "Teacher", "123.123.333-22", "active");
+var employ2 = new Employees("2", "2", "Gustavo", "03/10/1950", "M", "Administrator", "111.111.222-00", "active");
+var employ3 = new Employees("3", "3", "Evelyn", "01/06/1983", "F", "Coordinator", "333.333.333-33", "active");
+var employ4 = new Employees(sessionStorage.getItem("id"),
+    sessionStorage.getItem("user_id"),
+    sessionStorage.getItem("name"),
+    sessionStorage.getItem("bith_date"),
+    sessionStorage.getItem("sex"),
+    sessionStorage.getItem("type"),
+    sessionStorage.getItem("cpf"),
+    sessionStorage.getItem("status"));
+
+    console.log("nome:" + employ4.name);
+
+function registration() {
+    sessionStorage.setItem("id", document.getElementById("id").value);
+    sessionStorage.setItem("user_id", document.getElementById("user_id").value);
+    sessionStorage.setItem("name", document.getElementById("name").value);
+    sessionStorage.setItem("bith_date", document.getElementById("bith_date").value);
+    sessionStorage.setItem("sex", document.getElementById("sex").value);
+    sessionStorage.setItem("type", document.getElementById("type").value);
+    sessionStorage.setItem("cpf", document.getElementById("cpf").value);
+    sessionStorage.setItem("status", document.getElementById("status").value);
+
 }
 
-function show_birthdays()
-{
+
+function display_report_birthday() {
+
     
 }
+
+function display_employee_registers() {
+
+    
+}
+
