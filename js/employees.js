84b0882ccb3f7d7d4a47363bd6efac08ffe06b57
kalebe <kalebeadv66@gmail.com>
2021-11-20 22:40:58 -0300

    atualizando htmls e classes

--- js/employees.js
+++ js/employees.js
@@ -11,13 +11,7 @@ export class Employees extends Users {
 
 }
 
-let button_registration = document.getElementById("button_registration");
-let button_show_birthdays = document.getElementById("button_employee_birthday");
-
-button_registration.addEventListener("click", registration);
-
-button_show_birthdays.addEventListener("click", display_report_birthday);
-
+export const employees_guard = [employ1, employ2, employ3,employ4];
 
 var employ1 = new Employees("1", "1", "kalebe", "03/12/1960", "M", "Teacher", "123.123.333-22", "active");
 var employ2 = new Employees("2", "2", "Gustavo", "03/10/1950", "M", "Administrator", "111.111.222-00", "active");
@@ -30,30 +24,3 @@ var employ4 = new Employees(sessionStorage.getItem("id"),
     sessionStorage.getItem("type"),
     sessionStorage.getItem("cpf"),
     sessionStorage.getItem("status"));
-
-    
-export const employees_guard = [employ1,employ2,employ3,employ4];
-
-function registration() {
-    sessionStorage.setItem("id", document.getElementById("id").value);
-    sessionStorage.setItem("user_id", document.getElementById("user_id").value);
-    sessionStorage.setItem("name", document.getElementById("name").value);
-    sessionStorage.setItem("bith_date", document.getElementById("bith_date").value);
-    sessionStorage.setItem("sex", document.getElementById("sex").value);
-    sessionStorage.setItem("type", document.getElementById("type").value);
-    sessionStorage.setItem("cpf", document.getElementById("cpf").value);
-    sessionStorage.setItem("status", document.getElementById("status").value);
-
-}
-
-
-function display_report_birthday() {
-
-    
-}
-
-function display_employee_registers() {
-
-    
-}
-
