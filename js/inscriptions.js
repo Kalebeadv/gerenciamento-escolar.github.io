a847db90307549b60c01d29b727685a87ff37ccb
kalebe <kalebeadv66@gmail.com>
2021-11-21 20:28:35 -0300

    por enquanto, funcionando

--- js/inscriptions.js
+++ js/inscriptions.js
@@ -1,8 +1,3 @@
-import {students_guard} from "./students.js"
-import {notes_guard} from "./notes.js"
-import {matters_guard} from "./matters.js"
-import {courses_guard} from "./courses.js"
-
 class Inscriptions
 {
     constructor (id, course_id, matter_id, student_id, note_id)
@@ -13,25 +8,11 @@ class Inscriptions
         this.student_id = student_id;
         this.note_id = note_id;
     }
-    registration()
-    {
-
-    }
-    display_report()
-    {
-        
-    }
 }
 
-var inscription1 = new Inscriptions("1",courses_guard[0].id,matters_guard[0].id,"1",
-    notes_guard[0].id);
-var inscription2 = new Inscriptions("2",courses_guard[1].id,matters_guard[1].id,"2",
-notes_guard[1].id);
-
-var inscription3 = new Inscriptions("3",courses_guard[2].id,matters_guard[2].id,"3",
-notes_guard[1].id);
-
-var inscription4 = new Inscriptions("4",courses_guard[3].id,matters_guard[3].id,"4",
-notes_guard[1].id);
+var inscription1 = new Inscriptions("1","1","1","1","1");
+var inscription2 = new Inscriptions("2","2","2","2","2");
+var inscription3 = new Inscriptions("3","3","3","3","3");
+var inscription4 = new Inscriptions("4","4","4","4","4");
 
 export const inscriptions_guard = [inscription1,inscription2,inscription3,inscription4];
--- js/matters.js
+++ js/matters.js
@@ -1,19 +1,33 @@
-class Matters
-{
-    constructor(id,name,description)
-    {
+class Matters {
+    constructor(id, name, description) {
         this.id = id;
         this.name = name;
         this.description = description;
     }
+    atribute_array = ["id", "name", "description"];
+    element_array = ["4", "English", "Pro english"];
 
+    register_matters() {
+        for (let i = 0; i < atribute_array.length; i++) {
+            if (document.getElementById("id").value == null) {
+                sessionStorage.setItem(atribute_array[i], element_array[i]);
+                console.log(sessionStorage.getItem(atribute_array[i]));
+            }
+            else {
+                sessionStorage.setItem(atribute_array[i], document.getElementById(atribute_array[i]).value);
+                console.log(sessionStorage.getItem(atribute_array[i]));
+            }
+        }
+    }
 }
 
-var history = new Matters("1", "History", "The study of the past – specifically the people,"
+export const matters_guard = [history,mathematics,geography,english];
+
+var history = new Matters("0", "History", "The study of the past – specifically the people,"
     + "societies, events and problems of the past – as well as" 
     + "our attempts to understand them. It is a pursuit common to all human societies.");
 
-var mathematics = new Matters("2", "Mathematic", "mathematics, the science of structure" 
+var mathematics = new Matters("1", "Mathematic", "mathematics, the science of structure" 
     + "order, and relation that has evolved from"
     + "elemental practices of counting, measuring,"
     + "and describing the shapes of objects." 
@@ -21,14 +35,22 @@ var mathematics = new Matters("2", "Mathematic", "mathematics, the science of st
     + "and its development has involved an increasing degree of idealization"
     + "and abstraction of its subject matter.");
 
-var geography = new Matters("3", "Geography", "the study of the world, its physical and"
+var geography = new Matters("2", "Geography", "the study of the world, its physical and"
     + "social environments, and the dynamic nature of the relationships" 
     + "among them, from local to global scales.");
 
-var english = new Matters("4", "English", "English studies (usually called simply English)"
-    + "'is an academic' + discipline taught in primary, secondary, and post-secondary education" 
-    + "in English-speaking countries; it is not to be confused with English"
-    + "taught as a foreign language,which is a distinct discipline. It involves the" 
-    + "study and exploration of texts created in English literature.");
+var english = new Matters();
 
-export const matters_guard = [history,mathematics,geography,english];
+let button = document.getElementById("matter_register_class");
+ button.addEventListener("click",function()
+ {
+     localStorage.setItem("id", document.getElementById("id").value);
+     localStorage.setItem("name", document.getElementById("name").value);
+     localStorage.setItem("description", document.getElementById("description").value);
+     english = new Matters(localStorage.getItem("id"),localStorage.getItem("name"), 
+     localStorage.getItem("description"));
+     console.log(localStorage.getItem("id") + localStorage.getItem("name") +
+     localStorage.getItem("description"));
+ });
+
+
