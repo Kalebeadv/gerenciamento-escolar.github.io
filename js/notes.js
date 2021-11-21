1b4c058779aad4d74dc529bf2fea82b42748321f
kalebe <kalebeadv66@gmail.com>
2021-11-21 15:54:08 -0300

    Student funcionado oBS: pequeno bug na hora de exibir os nomes dos estudantes, se ele nao acha-los na lista

--- js/notes.js
+++ js/notes.js
@@ -10,9 +10,9 @@ class Notes
     }
 }
 
-var notes_students1 = new Notes("1", "1", "8.1", "8");
-var notes_students2 = new Notes("2", "2", "9.3", "9.2");
-var notes_students3 = new Notes("3", "3", "5.5", "10");
-var notes_students4 = new Notes("4","4","8.5","5.1");
+var notes_students1 = new Notes("1", "1", 8.1, 8);
+var notes_students2 = new Notes("2", "2", 9.3, 9.2);
+var notes_students3 = new Notes("3", "3", 5.5, 10);
+var notes_students4 = new Notes("4","4",8.5,5.1);
 
 export const notes_guard = [notes_students1,notes_students2,notes_students3,notes_students4];
--- js/students.js
+++ js/students.js
@@ -1,4 +1,5 @@
 import { Users } from "./users.js"
+import { notes_guard } from "./notes.js";
 
 export class Students extends Users
 {
@@ -11,9 +12,9 @@ export class Students extends Users
 
 
 
-var student1 = new Students("1", "1", "Alberto", "03/12/1960", "M")
-var student2 = new Students("2", "2", "Luciano", "03/10/1950", "M");
-var student3 = new Students("3", "3", "Emely", "01/06/1983", "F")
+var student1 = new Students("1", "1", "alberto", "03/12/1960", "M")
+var student2 = new Students("2", "2", "luciano", "03/10/1950", "M");
+var student3 = new Students("3", "3", "emely", "01/06/1983", "F")
 var student4 = new Students(sessionStorage.getItem("student_id"),
     sessionStorage.getItem("user_id_student"),
     sessionStorage.getItem("student_name"),
@@ -21,3 +22,68 @@ var student4 = new Students(sessionStorage.getItem("student_id"),
     sessionStorage.getItem("student_sex"));
 
 export const students_guard = [student1,student2,student3,student4];
+const atribute_array = ["student_id", "user_id_student", "student_name", "student_bith_date", "student_sex"];
+const element_array = ["4","4","Kalebe o iracundo", "03/12/2000", "M"];
+
+let show_notes = document.getElementById("enter_seach");
+show_notes.addEventListener("click", seach_notes);
+let register_student = document.getElementById("button__student_registration");
+register_student.addEventListener("click", register_students);
+
+function register_students()
+{
+    for(let i = 0; i < atribute_array.length; i++)
+    {
+        if(document.getElementById(atribute_array[i]).value == null)
+        {
+            sessionStorage.setItem(atribute_array[i],element_array[i]);
+            console.log(sessionStorage.getItem(atribute_array[i]));
+        }
+        else
+        {
+            sessionStorage.setItem(atribute_array[i], document.getElementById(atribute_array[i]).value);
+            console.log(sessionStorage.getItem(atribute_array[i]));
+        }
+    }
+    
+}
+
+function seach_notes()
+{
+    let input = document.getElementById("seach_notes");
+    for(let i = 0; i < students_guard.length; i++)
+    {
+        if(input.value == students_guard[i].name)
+        {
+            
+            if((notes_guard[i].first_assessment +  notes_guard[i].second_assessment) >= 7.0)
+            {
+               
+                window.alert(input.value + "\n" + "Approved\n" +
+                    "first_assessment: "+notes_guard[i].first_assessment + "\n" +
+                     "second_assessment: "+ notes_guard[i].second_assessment);
+                break;
+            }
+            else if((notes_guard[i].first_assessment +  notes_guard[i].second_assessment) < 7.0 && 
+                (notes_guard[i].first_assessment +  notes_guard[i].second_assessment) > 4.0 )
+            {
+
+                window.alert(input.value + "\n" + "Recovery\n" +
+                    "first_assessment: "+notes_guard[i].first_assessment + "\n" +
+                     "second_assessment: "+ notes_guard[i].second_assessment);
+                break;
+            }
+            else
+            {
+                window.alert(input.value + "\n" + "Failed\n" +
+                "first_assessment: "+notes_guard[i].first_assessment + "\n" +
+                "second_assessment: "+ notes_guard[i].second_assessment);
+                break;
+            }
+           
+        }
+        else{
+            window.alert("Students registers: " + students_guard[i].name);
+        }
+    }
+}
